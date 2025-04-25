f579ff60298eb5b58d451cf18ea8593669fa2e50
Sairam <sairammekala7@gmail.com>
2025-04-25 14:35:36 +0530

    Controlling drone programatically

--- Drone-simulation/drone2.py
+++ Drone-simulation/drone2.py
@@ -1,44 +1,89 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
+from pymavlink import mavutil
 import time
 
-# Connect to the Vehicle (adjust if using Mission Planner)
-vehicle = connect('udp:127.0.0.1:14552', wait_ready=True)  # Use 14550 for SITL/Mission Planner
-if (vehicle):
-    print("Connected to vehicle")
+# Connect to vehicle
+print("Connecting to vehicle...")
+vehicle = connect('udp:127.0.0.1:14552', wait_ready=True)
 
-def arm_and_takeoff(target_altitude):
+def clear_mission():
+    cmds = vehicle.commands
+    cmds.clear()
+    cmds.upload()
+
+def add_mission():
+    cmds = vehicle.commands
+    cmds.clear()
+
+    # List of waypoints (lat, lon, alt)
+    waypoints = [
+        (-35.363261, 149.165230, 10),  # WP1
+        (-35.363000, 149.166000, 10),  # WP2
+        (-35.362500, 149.165500, 10),  # WP3
+    ]
+
+    cmds.add(Command(  # TAKEOFF
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+        0, 0, 0, 0, 0, 0,
+        waypoints[0][0], waypoints[0][1], waypoints[0][2]
+    ))
+
+    for wp in waypoints:
+        cmds.add(Command(
+            0, 0, 0,
+            mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+            mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+            0, 0, 0, 0, 0, 0,
+            wp[0], wp[1], wp[2]
+        ))
+
+    cmds.add(Command(  # RTL
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0
+    ))
+
+    cmds.upload()
+    print("Mission uploaded.")
+
+def arm_and_takeoff(aTargetAltitude):
     print("Arming motors")
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
-
     while not vehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
 
     print("Taking off!")
-    vehicle.simple_takeoff(target_altitude)
+    vehicle.simple_takeoff(aTargetAltitude)
 
     while True:
-        print(f" Altitude: {vehicle.location.global_relative_frame.alt}")
-        if vehicle.location.global_relative_frame.alt >= target_altitude * 0.95:
+        print(" Altitude:", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
             print("Reached target altitude")
             break
         time.sleep(1)
 
-# --- Mission Execution ---
+# --- Run Mission ---
+clear_mission()
+add_mission()
+
 arm_and_takeoff(10)
 
-# Fly to a point (example coordinates)
-point1 = LocationGlobalRelative(-35.363261, 149.165230, 10)  # Change to your own coordinates
-vehicle.simple_goto(point1)
+print("Starting mission")
+vehicle.mode = VehicleMode("AUTO")
 
-# Wait while flying
-time.sleep(15)
+# Monitor until RTL or mission end
+while True:
+    print(" Mode:", vehicle.mode.name)
+    if vehicle.mode.name == "RTL":
+        print("Returning to Launch...")
+        break
+    time.sleep(2)
 
-# RTL
-print("Returning to Launch")
-vehicle.mode = VehicleMode("RTL")
-
-# Close vehicle object before exiting script
-time.sleep(10)
+# Close vehicle
 vehicle.close()
